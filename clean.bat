5da7812ac683252c6739a02e57988745ce7f1a8c
shlomobauer <shlomobauer@gmail.com>
2016-02-19 04:40:05 -0600

    Added files via upload
    initial upload.

---
+++ clean.bat
@@ -0,0 +1,5 @@
+del dbfw.xml
+del ifw.xml
+del efw.xml
+del buildscript.ps1
+del cleanup.ps1
---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -f dbfw.xml
+rm -f ifw.xml
+rm -f efw.xml
+rm -f buildscript.ps1
+rm -f cleanup.ps1
